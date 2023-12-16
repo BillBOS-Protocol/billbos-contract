9065036509fd3ac3be2786c9d6ef01e8c42cfad8
warunsinx <warunsinx@gmail.com>
2023-12-17 01:53:35 +0700

    feat: :zap: implemented BillBOSAdapter & MockCompound Contract

---
+++ contracts/interfaces/ICErc20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+interface ICErc20 {
+    function mint(uint256 mintAmount) external returns (uint);
+
+    function redeemUnderlying(uint redeemAmount) external returns (uint);
+
+    function balanceOfUnderlying(address owner) external view returns (uint256);
+}
