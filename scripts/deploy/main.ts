e27933b8607da30264d41b5b1b153d5f726a79ac
ribbinpo <ribbinpo@gmail.com>
2023-12-18 13:01:10 +0700

    feat: :sparkles:

--- scripts/deploy/main.ts
+++ scripts/deploy/main.ts
@@ -1,14 +1,16 @@
+import { ethers } from "hardhat";
 import { deployBBCompoundAdapter } from "./deployBBCompoundAdapter";
 import { deployBillBOSCore } from "./deployBillBOSCore";
 import { deployMockCompound } from "./mock/deployMockCompound";
 import { deployMockERC20 } from "./mock/deployMockERC20";
 
 async function main() {
-  const mockERC20 = await deployMockERC20();
-  const mockCompound = await deployMockCompound();
+  // const mockERC20 = await deployMockERC20();
+  // const mockCompound = await deployMockCompound();
   const billBOSCore = await deployBillBOSCore();
   const bBCompoundAdapter = await deployBBCompoundAdapter();
   await billBOSCore.setBillbosAdaptorAddress(await bBCompoundAdapter.getAddress());
+  // await mockERC20.mint(await mockCompound.getAddress(), ethers.parseEther("1000000"));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
