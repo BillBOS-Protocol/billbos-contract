b18166c50461b8704ba1864130e6340bbb7f3105
ribbinpo <ribbinpo@gmail.com>
2024-01-25 16:57:20 +0700

    feat: :wrench: update new address of billboscore

--- scripts/deploy/main.ts
+++ scripts/deploy/main.ts
@@ -1,20 +1,26 @@
 import { ethers } from "hardhat";
 import { deployBBCompoundAdapter } from "./deployBBCompoundAdapter";
 import { deployBillBOSCore } from "./deployBillBOSCore";
-import { deployMockCompound } from "./mock/deployMockCompound";
-import { deployMockERC20 } from "./mock/deployMockERC20";
+// import { deployMockCompound } from "./mock/deployMockCompound";
+// import { deployMockERC20 } from "./mock/deployMockERC20";
+import { getAddress } from "../../utils/address.util";
 
 async function main() {
-  const mockERC20 = await deployMockERC20();
-  const mockCompound = await deployMockCompound();
+  // const mockERC20 = await deployMockERC20();
+  // const mockCompound = await deployMockCompound();
   const billBOSCore = await deployBillBOSCore();
   const bBCompoundAdapter = await deployBBCompoundAdapter();
   await billBOSCore.setBillbosAdaptorAddress(
     await bBCompoundAdapter.getAddress()
   );
+
+  const mockERC20 = await ethers.getContractAt(
+    "MockERC20",
+    getAddress("mockERC20") ?? ""
+  );
   await mockERC20.mint(
-    await mockCompound.getAddress(),
-    ethers.parseEther("1000000")
+    getAddress("mockCompound") ?? "",
+    ethers.parseEther("100000000000")
   );
 }
 
